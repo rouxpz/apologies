516059648fbd3a7c8ff024f05a559ac4571e0206
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2020-12-17 21:12:19 -0500

    tts added

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,8 @@
-let changeColor = document.getElementById('amazon');
 let companies = document.getElementsByClassName('company');
+let companyNames = ['amazon', 'facebook', 'google'];
 let generated;
+
+var selectedVoices = [0, 10, 40];
 
 chrome.storage.sync.get('apologies', function(data) {
   for (var i = 0; i < companies.length; i++) {
@@ -16,19 +18,25 @@ chrome.storage.sync.get('apologies', function(data) {
 for (var i = 0; i < companies.length; i++) {
   companies[i].onclick = function() {
     var textToDisplay = this.id + "Text";
-    // alert(textToDisplay);
-    displayText(textToDisplay);
+    var ind = companyNames.indexOf(this.id);
+    // console.log(selectedVoices[ind]);
+    displayText(textToDisplay, selectedVoices[ind]);
     //clear from chrome storage
   }
 }
 
-function displayText(id) {
+function displayText(id, v) {
   var texts = document.getElementsByClassName('apologyText');
   for (var i = 0; i < texts.length; i++) {
     texts[i].style.display = "none";
   }
 
   document.getElementById(id).style.display = "block";
+  var voices = window.speechSynthesis.getVoices();
+  console.log(voices[v]);
+  var msg = new SpeechSynthesisUtterance(document.getElementById(id).innerHTML);
+  msg.voice = voices[v];
+  window.speechSynthesis.speak(msg);
 }
 
 //set timeout to display an alert with new apology
