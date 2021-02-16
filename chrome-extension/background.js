bd768a6dc2b32e2e4e4376560c417410ed97a91f
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2021-02-16 07:37:18 -0500

    updated dates and timing

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,7 +3,7 @@ var totalCompanies = [];
 var dates = [];
 var texts = [];
 var toSaySorry;
-let d = new Date();
+let d;
 let today;
 let checkTimer;
 let lastCheckedDate = '';
@@ -26,7 +26,7 @@ xmlhttp.open("GET", "data/apology-data.json", true);
 xmlhttp.send();
 
 chrome.runtime.onInstalled.addListener(function() {
-  checkTimer = setInterval(checkDate, 60000);
+  checkTimer = setInterval(checkDate, 120000);
   // console.log("Alarm set for " + timer + " minutes");
   // chrome.alarms.create("Start", {delayInMinutes:timer});
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -41,13 +41,13 @@ chrome.idle.onStateChanged.addListener(function(state) {
   if (state == 'active') {
     // console.log("last checked date: " + lastCheckedDate);
     clearInterval(checkTimer);
-    checkTimer = setInterval(checkDate, 60000);
+    checkTimer = setInterval(checkDate, 120000);
   }
 })
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.msg == 'restart date check') {
-    checkTimer = setInterval(checkDate, 60000);
+    checkTimer = setInterval(checkDate, 120000);
     chrome.storage.sync.set({apologies:[toSaySorry[0], toSaySorry[1], toSaySorry[2], true]}, function() {
       console.log('no new apologies available');
     });
@@ -57,15 +57,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 chrome.alarms.onAlarm.addListener(function() {
   alert("Today is February " + d.getDate() + ", 2021. You have a new apology waiting.");
   chrome.storage.sync.set({apologies:toSaySorry}, function() {
-    console.log('new apologies available');
+    // console.log('new apologies available');
   });
 });
 
 function checkDate() {
+  d = new Date();
   today = (d.getMonth() + 1) + '/' + d.getDate();
+  // console.log(today);
   toSaySorry = [];
   if (dates.indexOf(today) != -1 && lastCheckedDate != today) {
-    console.log("today's date at " + dates.indexOf(today));
+    // console.log("today's date at " + dates.indexOf(today));
 
     //adding data to toSaySorry array
     var index = dates.indexOf(today);
@@ -76,19 +78,19 @@ function checkDate() {
     // console.log(toSaySorry);
 
     //setting timer to launch alert
-    var newTimer =  Math.floor(Math.random() * 60) + 1;
+    var newTimer =  Math.floor(Math.random() * 1080) + 1;
     chrome.alarms.create("Second", {delayInMinutes: newTimer});
     // console.log(toSaySorry);
-    console.log("New alarm launched for " + newTimer + " minutes");
+    // console.log("New alarm launched for " + newTimer + " minutes");
 
     lastCheckedDate = today;
     // console.log("last checked date: " + lastCheckedDate);
     clearInterval(checkTimer);
   } else if (dates.indexOf(today) != -1 && lastCheckedDate == today){
-    console.log("date already checked today");
+    // console.log("date already checked today");
   } else if (d.getDate() >= 27) {
     clearInterval(checkTimer);
-    console.log("project period over");
+    // console.log("project period over");
   } else if (d.getDate() < 22) {
     console.log("waiting for project to start!");
   }
